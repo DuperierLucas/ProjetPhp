c55be1fa10c5c32f202630f7c6725687c5037583
BLANCO Manon <blancoma@hostname.iut2.upmf-grenoble.fr>
2018-10-17 16:25:14 +0200

    ajout dossier MVC + sql

---
+++ data/create.sql
@@ -0,0 +1,47 @@
+CREATE TABLE article (
+	libelle TEXT PRIMARY KEY,
+  description TEXT,
+  caracteristique TEXT,
+	categorie INTEGER,
+	prix REAL,
+	image TEXT,
+  nbStock INT,
+	FOREIGN KEY(categorie) REFERENCES categorie(id)
+);
+
+CREATE TABLE categorie (
+	nom TEXT PRIMARY KEY,
+);
+
+CREATE TABLE client (
+  id INT PRIMARY KEY,
+  nom TEXT,
+  prenom TEXT,
+  adresse TEXT,
+  mail TEXT,
+  motDePasse TEXT,
+)
+
+CREATE TABLE administrateur (
+  id INT PRIMARY KEY,
+  nom TEXT,
+  prenom TEXT,
+  mail TEXT,
+  motDePasse TEXT,
+)
+
+CREATE TABLE achat (
+  libelleArticle TEXT,
+  id INT,
+  PRIMARY KEY(libelleArticle, id),
+  FOREIGN KEY(libelleArticle) REFERENCES article(libelle),
+  FOREIGN KEY(id) REFERENCES client(id)
+)
+
+CREATE TABLE panier (
+  libelleArticle TEXT,
+  id INT,
+  PRIMARY KEY(libelleArticle, id),
+  FOREIGN KEY(libelleArticle) REFERENCES article(libelle),
+  FOREIGN KEY(id) REFERENCES client(id)
+)
