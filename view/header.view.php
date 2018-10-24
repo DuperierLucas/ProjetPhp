8d629b0a57924959f25c3e76ba6231d8a6b382e3
DUPERIER-CATALAZ Lucas <duperiel@hostname.iut2.upmf-grenoble.fr>
2018-10-24 17:39:25 +0200

    commit

--- view/header.view.php
+++ view/header.view.php
@@ -5,7 +5,7 @@
             <input type="text" name="recherche" value="Rechercher">
           </form>
           <ul>
-            <li> <a href="../controler/afficherCompte.ctrl.php"> <img src="../view/image/moncompte.jpg" alt="mon compte" > </a> </li>
+            <li> <a href="<?php//../controler/afficherCompte.ctrl.php?>../view/inscription.view.php"> <img src="../view/image/moncompte.jpg" alt="mon compte" > </a> </li>
             <li><a href="../controler/afficherPanier.ctrl.php"> <img src="../view/image/panier.jpg" alt="panier"> </a> </li>
           </ul>
         </article>
