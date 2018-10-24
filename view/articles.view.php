7f60cec70679ecaf72a9b892691d90e971ebe775
CABEZAS Mathias <cabezama@hostname.iut2.upmf-grenoble.fr>
2018-10-24 15:38:58 +0200

    vue article

--- view/articles.view.php
+++ view/articles.view.php
@@ -6,10 +6,22 @@
   </head>
   <body>
     <?php require_once('header.view.php'); ?>
-    <article>
-      <div>
-        
-      </div>
-    </article>
+    <!-- Une section contient l'ensemble des produit à afficher -->
+    <section>
+      <!-- Chaque div représente un article -->
+      <a href="#">
+        <div>
+          <img src="../view/image/vins/bourgogne.jpg.jpg" alt="bourgogne">
+          <!-- Nom du vin -->
+          <p>Bourgogne - Pinot Noir</p>
+          <!-- Description du vin -->
+          <p>Vin rouge francais </p>
+          <!-- caractéristique -->
+          <p>3% d'alcool</p>
+        </div>
+      </a>
+
+    </section>
+
   </body>
 </html>
