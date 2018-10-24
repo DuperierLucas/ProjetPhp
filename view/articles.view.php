6d299bf1ed6b02cba251696eb79900a2635b91ba
BLANCO Manon <blancoma@hostname.iut2.upmf-grenoble.fr>
2018-10-24 17:29:41 +0200

    ok

--- view/articles.view.php
+++ view/articles.view.php
@@ -8,11 +8,17 @@
   <body>
     <?php require_once('../view/header.view.php'); ?>
     <!-- Une section contient l'ensemble des produit à afficher -->
+
+    <!-- Si un produit a été commandé on affiche un message de sa prise en compte -->
+    <?php if ($commande) : ?>
+    <p>Votre produit a été ajouté au panier</p>
+    <?php endif?>
+
     <section>
       <?php foreach ($articles as $value) :?>
       <!-- Chaque div représente un article -->
       <!-- lorsque clique sur le lien ajoute au panier : cookie ?-->
-      <a href="#">
+      <a href="../controler/afficherArticles.ctrl.php?categorie=<?= $categorie?>&article=<?= $value->ref ?>">
         <div>
           <img src="../view/image/vins/<?= $value->image ?>" alt="<?= $value->libelle?>">
           <!-- Nom du vin -->
