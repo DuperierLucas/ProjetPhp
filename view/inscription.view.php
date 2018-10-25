ebfb5d57541c139baa96569df18f9d1edce9d867
CABEZAS Mathias <cabezama@hostname.iut2.upmf-grenoble.fr>
2018-10-25 18:38:47 +0200

    divers correctionet amélioration de style

--- view/inscription.view.php
+++ view/inscription.view.php
@@ -8,52 +8,43 @@
   <body>
     <?php require_once('../view/header.view.php'); ?>
 
-    <?php if(isset($msgErreur)) : ?>
-      <font color="red"><p><?= $msgErreur ?></p></font>
-    <?php endif ?>
 
-    <form action="../controler/afficherNewUser.ctrl.php" method="post">
-      <fieldset>
-        <legend>Formulaire d'inscription</legend>
+    <section>
+      <form action="../controler/afficherNewUser.ctrl.php" method="post">
+        <fieldset>
+          <legend>Formulaire d'inscription</legend>
+            <?php if(isset($msgErreur)) : ?>
+              <p class="erreur"><?= $msgErreur ?></p>
+            <?php endif ?>
+            <label for="nom">Nom * :</label>
+            <input type="text" name="nm" size="10" id="nom"  required/>
 
-          <label for="nom">Nom * :</label>
-          <input type="text" name="nm" size="10" id="nom"  required/>
-          </br>
+            <label for="prenom">Prénom * :</label>
+            <input type="text" name="pr" size="75" id="prenom" required/>
 
-          <label for="prenom">Prénom * :</label>
-          <input type="text" name="pr" size="75" id="prenom" required/>
-          </br>
+            <label for="mail1">E-mail * :</label>
+            <input type="Email" name="mel1" size="100" id="mail1" required/>
 
-          <label for="mail1">E-mail * :</label>
-          <input type="Email" name="mel1" size="100" id="mail1" required/>
-          </br>
+            <label for="mail2">Confirmation de l'e-mail * :</label>
+            <input type="Email" name="mel2" size="100" id="mail2" required/>
 
-          <label for="mail2">Confirmation de l'e-mail * :</label>
-          <input type="Email" name="mel2" size="100" id="mail2" required/>
-          </br>
+            <label for="mdp">Choisir un mot de passe * :</label>
+            <input type="password" name="ajzt" size="100" id="mdp" required/>
 
-          <label for="mdp">Choisir un mot de passe * :</label>
-          <input type="password" name="ajzt" size="100" id="mdp" required/>
-          </br>
+            <label for="mdp2">Confirmation du mot de passe * :</label>
+            <input type="password" name="pdsf" size="100" id="mdp2" required/>
 
-          <label for="mdp2">Confirmation du mot de passe * :</label>
-          <input type="password" name="pdsf" size="100" id="mdp2" required/>
-          </br>
+            <label for="adresse">Adresse * :</label>
+            <input type="text" name="adr" size="100" id="adresse" required placeholder="300 rue des champs 38000 GRENOBLE"/>
 
-          <label for="adresse">Adresse * :</label>
-          <input type="text" name="adr" size="100" id="adresse" required placeholder="300 rue des champs 38000 GRENOBLE"/>
-          </br>
+            <label for="telephone">Téléphone * :</label>
+            <input type="integer" name="tel" size="10" id="telephone" required/>
+            <p>
+                <input class="bouton" type="submit" value="Valider"/>
+            </p>
+        </fieldset>
+      </form>
+    </section>
 
-          <label for="telephone">Téléphone * :</label>
-          <input type="integer" name="tel" size="10" id="telephone" required/>
-          </br>
-
-      </fieldset>
-
-      <p>
-          <input type="submit" value="Valider"/>
-          <input type="reset"  value="Supprimer"/> </p>
-
-    </form>
   </body>
 </html>
