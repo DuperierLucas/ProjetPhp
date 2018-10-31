686dd69c8b0802f618dab58a5afc8151216cc737
CABEZAS Mathias <cabezama@hostname.iut2.upmf-grenoble.fr>
2018-10-31 16:36:41 +0100

    petites retouches css

--- controler/afficherCompte.ctrl.php
+++ controler/afficherCompte.ctrl.php
@@ -26,7 +26,7 @@ if(isset($_SESSION['id'])) {
 
 //Si l'utilisateur à modifier son compte, on enregistre les modifications dans la base
 if(!empty($_POST['enregistrer'])) {
-  modifierClient($client->id, $_POST['nom'], $_POST['prenom'], $_POST['adresse'], $_POST['telephone'], $_POST['mail']);
+  $dao->modifierClient($client->id, $_POST['nom'], $_POST['prenom'], $_POST['adresse'], $_POST['telephone'], $_POST['mail']);
 }
 
 
--- view/footer.view.php
+++ view/footer.view.php
@@ -2,4 +2,5 @@
   <p>Alcooliques&Anonymes</p>
   <p>Fondé par Manon Blanco, Lucas Duperier, Mathias Cabezas</p>
   <p>Inspiré de <a href="https://lesnouveauxcavistes.com/"> https://lesnouveauxcavistes.com/</a> </p>
+  <p> L'abus d'alcool est dangereux pour la santé, A consommer avec modération. </p>
 </footer>
